066cef4c67115214d8ebef1bcb30e8a40945bc89
JosuePercy <josuehuallullo12@gmail.com>
2024-03-28 10:17:32 -0500

    initial project, searching movies

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useRef } from 'react';
+import './App.css'
+import { Movies } from './components/Movies';
+import { useMovies } from './hooks/useMovies';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function useSearch() {
+
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  /*   const handleSubmit = ( event ) => {
+    event.preventDefault()
+    const fields = Object.fromEntries( new window.FormData(event.target)) => TODO: forma no contralada
+    console.log(fields);
+  }; */
+
+  useEffect(() => {
+
+    if ( isFirstInput.current ) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacia");
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una pelicula con un número");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La busqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { 
+    search,
+    updateSearch,
+    error,
+  };
+
+
+
+}
+function App() {
+  const [ sort, setSort ] = useState(false)
+  const { search, updateSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     getMovies({ search })
+   };
+
+   const handleSort = () => {
+     setSort(!sort)
+   }
+
+   const handleChange = (event) => {
+     updateSearch(event.target.value);
+   };
+
+
+  return (
+    <>
+      <div className="page">
+        <header>
+          <h1>Buscardor de películas</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              style={{
+                border: "1px solid transparent",
+                borderColor: error ? "red" : "transparent",
+              }}
+              value={search}
+              onChange={handleChange}
+              name="query"
+              placeholder="Avengers, Star Wars, The Matrix..."
+            />
+            <input type='checkbox' onChange={ handleSort } checked={sort} />
+            <button type="submit">Buscar</button>
+          </form>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </header>
+
+        <main>
+        {
+          loading 
+          ? <p>Cargando...</p> 
+          : <Movies movies={movies} />
+        }
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/hooks/useMovies.js
@@ -0,0 +1,50 @@
+import withoutResults from "../mocks/no-results.json";
+import { useRef, useState, useMemo, useEffect, useCallback } from "react";
+import { searchMovies } from "../services/movies";
+
+export function useMovies({ search , sort }) {
+
+  const [ movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previusSearch = useRef(search)
+
+  const getMovies = useCallback( async ({ search }) => {
+      if (search === previusSearch.current) return;
+      try {
+        setLoading(true);
+        setError(null);
+        previusSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+  }, []); 
+
+  useEffect( () => {
+    console.log('new getMovies received')
+  }, [getMovies])
+
+
+/*   const getSortedMovies = () => {
+    console.log("getSortedMovies");
+    const sortedMovies = sort
+      ? [...movies ].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+
+    return sortedMovies
+  }
+ */
+
+  const sortedMovies = useMemo( () => {
+    console.log('memoSortedMovies')
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [ sort, movies ])
+
+  return { movies: sortedMovies, getMovies, loading, sort };
+}
